f66e1b509fbc4232e1ab9da2b5a05c0c261b8216
Fedorchenko Anton <antony.corwin@gmail.com>
2019-05-25 23:23:56 +0300

    add build dir

---
+++ build/precache-manifest.40a1ed4d6b2492d1eb298fc10c411ad7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d92f35f1efebc38103b8",
+    "url": "/goit-react-hw-02/static/js/runtime~main.577bbb3b.js"
+  },
+  {
+    "revision": "53b1343847f20a59f5bd",
+    "url": "/goit-react-hw-02/static/js/main.8fa7fd47.chunk.js"
+  },
+  {
+    "revision": "607e1b76a734c9346e55",
+    "url": "/goit-react-hw-02/static/js/2.3cac6f01.chunk.js"
+  },
+  {
+    "revision": "53b1343847f20a59f5bd",
+    "url": "/goit-react-hw-02/static/css/main.2f6b7fe6.chunk.css"
+  },
+  {
+    "revision": "941a44a2df87f729817c1846b77f49db",
+    "url": "/goit-react-hw-02/index.html"
+  }
+];
